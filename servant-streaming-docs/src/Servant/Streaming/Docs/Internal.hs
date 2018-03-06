472346ec69f90a4dbeb04b11ebce681c0e9a048d
Julian K. Arni <jkarni@gmail.com>
2018-03-06 16:28:34 +0100

    Docs for StreamResponse

--- servant-streaming-docs/src/Servant/Streaming/Docs/Internal.hs
+++ servant-streaming-docs/src/Servant/Streaming/Docs/Internal.hs
@@ -6,6 +6,7 @@ import           Control.Lens
 import           Data.Proxy               (Proxy (Proxy))
 import           Servant.API              hiding (Stream)
 import           Servant.API.ContentTypes (allMime, AllMime)
+import           GHC.TypeLits
 import           Servant.Docs
 import           Servant.Streaming
 
@@ -20,3 +21,17 @@ instance (HasDocs subapi, AllMime contentTypes)
 
       action' = action & rqbody .~ (bodyFor <$> mimes)
                        & rqtypes .~ mimes
+
+instance (AllMime contentTypes, KnownNat status, ReflectMethod method)
+    => HasDocs (StreamResponse method status contentTypes) where
+  docsFor _ (ep, action) DocOptions{..} =
+    single endpoint action'
+
+    where endpoint = ep & method .~ method'
+          action' = action & response.respBody .~ (bodyFor <$> mimes)
+                           & response.respTypes .~ mimes
+                           & response.respStatus .~ status
+          mimes = allMime (Proxy :: Proxy contentTypes)
+          bodyFor x = ("Streaming Body", x, "--some streaming body--")
+          method' = reflectMethod (Proxy :: Proxy method)
+          status = fromInteger $ natVal (Proxy :: Proxy status)
